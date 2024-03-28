0b3b007822a3a81a34556a9f66c511a972bcce1f
Danil-114195722 <ejyou.user@gmail.com>
2024-03-28 21:49:22 +0300

    Started to make html editor

---
+++ static/js/note.js
@@ -0,0 +1,11 @@
+//const save_button = document.getElementById('save');
+//const logout_button = document.getElementById('logout');
+//const all_notes_button = document.getElementById('all_notes');
+
+
+// при нажатии на кнопку save выполняем созранение введённого текста
+function saveFunc () {
+    // записываем содержимое нашего редактора в переменную
+    const content = document.getElementById('editor').innerHTML;
+    console.log(content);
+};
