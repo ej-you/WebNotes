044998f95a86e32246f91259d8d0a2eec0571e49
Danil-114195722 <ejyou.user@gmail.com>
2024-03-28 22:24:07 +0300

    Added simple selection getter

--- static/js/note.js
+++ static/js/note.js
@@ -1,11 +1,27 @@
-//const save_button = document.getElementById('save');
-//const logout_button = document.getElementById('logout');
-//const all_notes_button = document.getElementById('all_notes');
-
-
 // при нажатии на кнопку save выполняем созранение введённого текста
 function saveFunc () {
     // записываем содержимое нашего редактора в переменную
     const content = document.getElementById('editor').innerHTML;
     console.log(content);
+};
+
+
+function getSelectionText () {
+    let text = "";
+    // Не IE - используем метод getSelection (IE - используем объект selection [пока возвращаем ничего])
+    if (text = window.getSelection) {
+        text = window.getSelection().toString();
+        console.log("window.getSelection()", window.getSelection());
+    } else {
+        // text = document.selection.createRange().text;
+        return false;
+    };
+
+    return text;
+};
+
+
+function wrapTag(button) {
+    console.log("button.id", button.id);
+    console.log("getSelectionText()", [getSelectionText()]);
 };
