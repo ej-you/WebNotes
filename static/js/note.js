8019535dc0e7b9cf0e35f87d64eee3ea0d3b26c2
Danil-114195722 <ejyou.user@gmail.com>
2024-04-02 21:45:10 +0300

    Added buttons to make italic/bold text and undo/redo buttons

--- static/js/note.js
+++ static/js/note.js
@@ -1,8 +1,28 @@
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    var content = document.getElementById('editor').innerHTML;
+    let content = document.getElementById('editor').innerHTML;
     // Устанавливаем значение скрытого поля
     document.getElementById('content').value = content;
     // Отправляем форму
     this.submit();
 });
+
+
+function wrapText(button) {
+    console.log(button.id);
+
+    switch (button.id) {
+        case ('b'):
+            document.execCommand("bold");
+            break;
+        case ('i'):
+            document.execCommand("italic");
+            break;
+        case ('undo'):
+            document.execCommand("undo");
+            break;
+        case ('redo'):
+            document.execCommand("redo");
+            break;
+    };
+}
