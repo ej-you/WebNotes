be2dbbcc16844652676bd60fa6397179d8474250
Danil-114195722 <ejyou.user@gmail.com>
2024-04-05 09:26:29 +0300

    Update flask run

--- app.py
+++ app.py
@@ -26,5 +26,4 @@ app.add_url_rule("/logout", methods=["GET", "POST"], view_func=views.logout)
 
 
 if __name__ == "__main__":
-    # app.run(debug=True, host='0.0.0.0', port='5000')
-    app.run(debug=False, host='0.0.0.0', port='8080')
+    app.run(debug=False, host='0.0.0.0', port='5001')
