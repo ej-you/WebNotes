36a3e403c10ece367ca292f74a3046e15f5b3dd7
Danil-114195722 <ejyou.user@gmail.com>
2024-09-12 23:16:57 +0300

    Added "clear format" func

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ app.add_url_rule("/logout", methods=["GET", "POST"], view_func=views.logout)
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port='5001')
+    app.run(debug=True, host='0.0.0.0', port='5001')
--- static/js/note.js
+++ static/js/note.js
@@ -1,6 +1,8 @@
+const editorElem = document.getElementById('editor');
+
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    let content = document.getElementById('editor').innerHTML;
+    let content = editorElem.innerHTML;
     // Устанавливаем значение скрытого поля
     document.getElementById('content').value = content;
     // Отправляем форму
@@ -23,6 +25,27 @@ function wrapText(button) {
             break;
         case ('redo'):
             document.execCommand("redo");
+            break;
+        case ('clearFormat'):
+            document.execCommand("backColor", false, "white");
+            document.execCommand("foreColor", false, "black");
+
+            // Если включен жирный текст на текущем выделении - отключаем
+            if (document.queryCommandState("bold")) {
+                document.execCommand("bold");
+            }
+            // Если включен курсив на текущем выделении - отключаем
+            if (document.queryCommandState("italic")) {
+                document.execCommand("italic");
+            }
+
+//            const editorFontSize = window.getComputedStyle(editorElem).fontSize
+//
+//            const spans = document.querySelectorAll('#editor span');
+//            spans.forEach(span => {
+//                span.style.fontSize = editorFontSize;
+//            });
+
             break;
     };
 }
